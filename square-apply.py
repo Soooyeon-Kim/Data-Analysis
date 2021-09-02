435c7c493d5de0706216d2df81f766ae697e5906
Soooyeon-Kim <83583349+Soooyeon-Kim@users.noreply.github.com>
2021-09-02 15:08:18 +0900

    Add files via upload

---
+++ square-apply.py
@@ -0,0 +1,19 @@
+import numpy as np
+import pandas as pd
+
+df = pd.DataFrame(np.arange(5), columns=["Num"])
+print(df, "\n")
+
+# 값을 받으면 제곱을 해서 돌려주는 함수
+def square(x):
+    return x*x
+
+
+# apply로 컬럼에 함수 적용
+df['Square'] = df['Num'].apply(square)
+print(df)
+
+
+# 람다 표현식으로도 적용하기
+df['Square'] = df['Num'].apply(lambda x : x * x)
+print(df)
